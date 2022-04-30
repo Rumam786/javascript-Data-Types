e6cb8e1b55dc8b0beb63ecae763d4d1ee9740306
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-04-30 23:30:00 +0500

    Add files via upload

---
+++ family.js
@@ -0,0 +1,24 @@
+var a = "hello world";
+b = 34;
+c = true;
+d = ["html","css","js"];
+e = {
+    Name : "Ruman",
+    country : "pakistan"
+};
+f = null;
+var g;
+document.write("<br>");
+document.write(typeof a);
+document.write("<br>");
+document.write(typeof b);
+document.write("<br>");
+document.write(typeof c);
+document.write("<br>");
+document.write(typeof d);
+document.write("<br>");
+document.write(typeof e);
+document.write("<br>");
+document.write(typeof f);
+document.write("<br>");
+document.write(typeof g);
